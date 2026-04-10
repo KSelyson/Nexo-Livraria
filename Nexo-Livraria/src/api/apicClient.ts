34ca1d6eb650ef3300fe818cde652a32c1aa95a8
kselyson <kauanselyson13@gmail.com>
2026-04-10 15:05:19 -0300

    fix: corrige instância do axios

--- Nexo-Livraria/src/api/apicClient.ts
+++ Nexo-Livraria/src/api/apicClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Cria uma instância customizada do Axios
 const apiClient = axios.create({
   // Usa o proxy /api configurado no vite.config.ts
-  baseURL: '/api', 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
